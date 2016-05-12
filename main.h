2f29de99f58e978ff173e174176610b40d08f204
mimos1010 <zwitztheone@gmail.com>
2016-05-11 20:28:31 -0400

    Create main.h

---
+++ main.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <SFML/Graphics.hpp>
+#include <stdlib.h>
+#include <iostream>
+#include <string.h>
+#include <cstdlib>
+#include <wtypes.h>
+#include <vector>
+
+#include "Engine.h"
+
+using namespace sf;
+using namespace std;
